f8cf03eca0037163d3726fca18ba1025ab1f1a9f
r3v0h4n <larevoo@gmail.com>
2024-04-27 13:00:36 +0300

    task1

---
+++ task1/main.py
@@ -0,0 +1,24 @@
+import pathlib
+
+
+def parse_salary(line):
+    return float(line.split(",")[1])
+
+def total_salary(path):
+    current_dir = pathlib.Path(__file__).parent
+
+    try:
+        with open(current_dir / path, "r", encoding="UTF-8") as file:        
+            salaries = [parse_salary(line) for line in file.read().splitlines()]
+    except Exception:
+        print("Error occured")
+        return 0, 0
+    
+    salary_sum = sum(salaries)
+    avarage_salary = salary_sum / len(salaries)
+    return salary_sum, avarage_salary
+
+
+if __name__ == "__main__":
+    total, average = total_salary("salaries.txt")
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
